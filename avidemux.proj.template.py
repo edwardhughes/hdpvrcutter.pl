cdb267f4f11b462e0247677dcfb23d054f647a6c
Edward Hughes IV <edward@edwardhughes.org>
2011-07-17 19:33:30 -0400

    Removed some javascript bits from the python project template.

--- avidemux.proj.template.py
+++ avidemux.proj.template.py
@@ -10,13 +10,12 @@ app.clearSegments()
 APPADDSEGMENT
 #** Postproc **
 app.setPostProc(3,3,0)
-#app.video.fps1000 = FPS1000
 #** Filters **
 #** Video Codec conf **
 app.videoCodec("copy")
 #** Audio **
-#app.audioReset()
-#app.audioCodec("copy",0)
+app.audioReset()
+app.audioCodec("copy",0)
 #** Container **
 app.setContainer("AVI")
 
